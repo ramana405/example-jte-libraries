7baea3d6882bc1fd9f1ffcbf0fdfe63bdf64bda5
ramana405 <41909526+ramana405@users.noreply.github.com>
2019-07-08 17:48:26 +0530

    Update build.groovy

--- build/build.groovy
+++ build/build.groovy
@@ -4,7 +4,7 @@ stage('build') {
        echo "Helloooooooooooooooooo"
        node {
               bat '"C:\\ProgramData\\chocolatey\\lib\\NuGet.CommandLine\\tools\\nuget.exe" restore DancingGoat.sln'
-              bat "\"${tool 'MSBuild'}\" DancingGoat.sln /p:Configuration=Release /p:Platform=\"Any CPU\" /P:DeployOnBuild=true /p:ProductVersion=1.0.${env.BUILD_NUMBER}"
+              bat "\"${tool 'MSBuild'}\" DancingGoat.sln /p:Configuration=Release /p:Platform=\"Any CPU\" /P:DeployOnBuild=true"
        }
   } 
 echo "ending msbuild"
