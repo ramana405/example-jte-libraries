4c405c65e54fc09d40047fe532247f5c206dd9a5
ramana405 <41909526+ramana405@users.noreply.github.com>
2019-07-08 15:10:33 +0530

    Update upload_artifact.groovy

--- upload_artifact/upload_artifact.groovy
+++ upload_artifact/upload_artifact.groovy
@@ -5,8 +5,8 @@ stage("Upload_Artifact"){
     node{
         sleep(5)
         /*withAWS(region:'us-east-2',credentials:'<Credentials_Id>') {
-        s3Upload(file:"C:\\users\\venkataramana.t\\.jenkins\\workspace\\dotnet_project\\DemoApp\\obj\\release\\package\\DemoApp-${env.BUILD_NUMBER}.zip", bucket:'<bucket-name>')*/
-        }
+        s3Upload(file:"C:\\users\\venkataramana.t\\.jenkins\\workspace\\dotnet_project\\DemoApp\\obj\\release\\package\\DemoApp-${env.BUILD_NUMBER}.zip", bucket:'<bucket-name>')
+        }*/
     }
 }
 echo "Artifacts Uploaded to S3 bucket"
