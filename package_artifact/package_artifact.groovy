41a925aba54709f1acf99c229edcaf6402664ae6
ramana405 <41909526+ramana405@users.noreply.github.com>
2019-07-08 16:37:20 +0530

    Update package_artifact.groovy

--- package_artifact/package_artifact.groovy
+++ package_artifact/package_artifact.groovy
@@ -2,7 +2,7 @@ void call(){
 echo "Starting Package Stage"
 stage("Package"){
        node{
-              bat "\"${tool 'MSBuild'}\" DancingGoat.sln /T:Clean;Build;Package  /p:Configuration=Release /p:Platform=\"Any CPU\" /p:DeployOnBuild=true /p:ProductVersion=1.0.0.${env.BUILD_NUMBER}"
+              bat "\"${tool 'MSBuild'}\" DancingGoat.sln /T:Build;Package  /p:Configuration=Release /p:Platform=\"Any CPU\" /p:DeployOnBuild=true /p:ProductVersion=1.0.0.${env.BUILD_NUMBER}"
               bat "rename C:\\Users\\venkataramana.t\\.jenkins\\workspace\\DancingGoat\\DancingGoat\\obj\\Release\\Package\\DancingGoat.zip DancingGoat-${env.BUILD_NUMBER}.zip" 
        }
 }
