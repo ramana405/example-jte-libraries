5fa7bcac36db6c7506eb4b52834eced1d01743ab
ramana405 <41909526+ramana405@users.noreply.github.com>
2019-07-08 18:20:12 +0530

    Adding Sleep stage

--- deploy_qa/deploy_qa.groovy
+++ deploy_qa/deploy_qa.groovy
@@ -3,10 +3,12 @@ void call(){
 echo "Starting Deployment"
 stage("Deploy_QA"){
 	node{
+	     echo "Deploying to QA"
+	     sleep(5)	
               /*mail to: 'venkataramana.thalari@cummins.com',
               subject: "Job '${env.JOB_NAME}' (${env.BUILD_NUMBER}) is waiting for input",
-              body: "Please go to ${env.BUILD_URL}console and verify the build"*/
-              input "Deploy to Production?"
+              body: "Please go to ${env.BUILD_URL}console and verify the build"
+              input "Deploy to Production?"*/
 	      //Deployment Script
     }
 }
