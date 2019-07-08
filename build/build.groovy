fcca94dffd4c2fd970330c88b304689ce939904d
ramana405 <41909526+ramana405@users.noreply.github.com>
2019-07-08 17:56:54 +0530

    Update build.groovy

--- build/build.groovy
+++ build/build.groovy
@@ -1,10 +1,10 @@
 void call() {
 echo "starting msbuild"
 stage('build') {
-       echo "Helloooooooooooooooooo"
        node {
-              bat '"C:\\ProgramData\\chocolatey\\lib\\NuGet.CommandLine\\tools\\nuget.exe" restore DancingGoat.sln'
-              bat "\"${tool 'MSBuild'}\" DancingGoat.sln /p:Configuration=Release /p:Platform=\"Any CPU\" /P:DeployOnBuild=true"
+              echo "Code build is in progress" 
+              //bat '"C:\\ProgramData\\chocolatey\\lib\\NuGet.CommandLine\\tools\\nuget.exe" restore DancingGoat.sln'
+              //bat "\"${tool 'MSBuild'}\" DancingGoat.sln /p:Configuration=Release /p:Platform=\"Any CPU\" /P:DeployOnBuild=true"
        }
   } 
 echo "ending msbuild"
