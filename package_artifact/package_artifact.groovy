ffa4a0ff5e0f3eee551991eaa7921ebd8eb5e8fd
ramana405 <41909526+ramana405@users.noreply.github.com>
2019-07-08 17:57:51 +0530

    Update package_artifact.groovy

--- package_artifact/package_artifact.groovy
+++ package_artifact/package_artifact.groovy
@@ -2,8 +2,9 @@ void call(){
 echo "Starting Package Stage"
 stage("Package"){
        node{
-              "\"${tool 'MSBuild'}\" DancingGoat.sln /p:Configuration=Release /p:Platform=\"Any CPU\" /P:DeployOnBuild=true /p:ProductVersion=1.0.${env.BUILD_NUMBER}"
-              bat "rename C:\\Users\\venkataramana.t\\.jenkins\\workspace\\JenkinsTemplateEngineDemo_master\\DancingGoat\\obj\\Release\\Package\\DancingGoat.zip DancingGoat-${env.BUILD_NUMBER}.zip"
+              echo "packaging the code is in progress.."
+              //"\"${tool 'MSBuild'}\" DancingGoat.sln /p:Configuration=Release /p:Platform=\"Any CPU\" /P:DeployOnBuild=true /p:ProductVersion=1.0.${env.BUILD_NUMBER}"
+              //bat "rename C:\\Users\\venkataramana.t\\.jenkins\\workspace\\JenkinsTemplateEngineDemo_master\\DancingGoat\\obj\\Release\\Package\\DancingGoat.zip DancingGoat-${env.BUILD_NUMBER}.zip"
        }
 }
 echo "Ending Package Stage"      
