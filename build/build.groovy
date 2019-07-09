e4a832c09a9be97597a042583b057837bb58b8dd
ramana405 <41909526+ramana405@users.noreply.github.com>
2019-07-09 17:05:03 +0530

    Update build.groovy

--- build/build.groovy
+++ build/build.groovy
@@ -4,7 +4,7 @@ stage('build') {
        node {
               echo "Code build is in progress" 
               input id: 'Abc', message: 'Enter the solution file name?', ok: 'Continue', parameters: [string(defaultValue: 'DancingGoat.sln', description: '', name: 'Solution', trim: false)]
-              parameters: [[$class: 'StringParameterValue', name: 'target', value: target], [$class: 'ListSubversionTagsParameterValue', name: 'release', tag: release], [$class: 'BooleanParameterValue', name: 'update_composer', value: update_composer]]
+              parameters ([[$class: 'StringParameterValue', name: 'target', value: target], [$class: 'ListSubversionTagsParameterValue', name: 'release', tag: release], [$class: 'BooleanParameterValue', name: 'update_composer', value: update_composer]])
               echo "${params.Solution}"
               echo "${params.release}"
               echo "${env.JOB_NAME}"
