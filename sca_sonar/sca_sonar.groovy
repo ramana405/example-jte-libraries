36bebf0a4774637c88f139f5f7caf5397457211b
ramana405 <41909526+ramana405@users.noreply.github.com>
2019-07-09 18:43:27 +0530

    Update sca_sonar.groovy

--- sca_sonar/sca_sonar.groovy
+++ sca_sonar/sca_sonar.groovy
@@ -3,10 +3,10 @@ echo "Starting Static Code Analysis"
 stage('SCA_Sonar'){
        node{
             echo "Static code Analysis is in progress.."
-            sleep(5)  
-          /*bat '"C:\\ProgramData\\chocolatey\\bin\\SonarScanner.MSBuild.exe" begin /k:DancingGoat'
-        	bat '"C:\\Program Files (x86)\\Microsoft Visual Studio\\2019\\Community\\MSBuild\\Current\\Bin\\MSBuild.exe" DancingGoat.sln /t:Rebuild'
-        	bat '"C:\\ProgramData\\chocolatey\\bin\\SonarScanner.MSBuild.exe" end'*/
+            //sleep(5)  
+            bat '"C:\\ProgramData\\chocolatey\\bin\\SonarScanner.MSBuild.exe" begin /k:DancingGoat'
+            bat '"C:\\Program Files (x86)\\Microsoft Visual Studio\\2019\\Community\\MSBuild\\Current\\Bin\\MSBuild.exe" ${params.Solution_file} /t:Rebuild'
+            bat '"C:\\ProgramData\\chocolatey\\bin\\SonarScanner.MSBuild.exe" end'
        }
 } 
 echo "Ending Static Code Analysis"
