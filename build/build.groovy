57ec9434a4a030bad789f36bdecf93a0246d2365
ramana405 <41909526+ramana405@users.noreply.github.com>
2019-07-09 17:25:18 +0530

    Update build.groovy

--- build/build.groovy
+++ build/build.groovy
@@ -2,20 +2,17 @@ void call() {
 echo "starting msbuild"
 stage('build') {
        node {
-              echo "Code build is in progress" 
-              input id: 'Abc', message: 'Enter the solution file name?', ok: 'Continue', parameters: [string(defaultValue: 'DancingGoat.sln', description: '', name: 'Solution', trim: false)]
+              echo "Code build is in progress"
               properties([
   	              parameters([
-  		              string(name: 'mybranch', defaultValue: 'undefined')
+  		              string(name: 'Solution_file', defaultValue: 'undefined')
   	              ])
               ])
-              println "Hello from the shebang line"
               println "####Printing parameter:"
-              echo "mybranch = ${params.mybranch}"
-              echo "${params.Solution}"
+              echo "mybranch = ${params.Solution_file}"
               echo "${env.JOB_URL}"
-              //bat '"C:\\ProgramData\\chocolatey\\lib\\NuGet.CommandLine\\tools\\nuget.exe" restore DancingGoat.sln'
-              //bat "\"${tool 'MSBuild'}\" DancingGoat.sln /p:Configuration=Release /p:Platform=\"Any CPU\" /P:DeployOnBuild=true"
+              bat '"C:\\ProgramData\\chocolatey\\lib\\NuGet.CommandLine\\tools\\nuget.exe" restore ${params.Solution_file}'
+              bat "\"${tool 'MSBuild'}\" ${params.Solution_file} /p:Configuration=Release /p:Platform=\"Any CPU\" /P:DeployOnBuild=true"
        }
   } 
 echo "ending msbuild"
