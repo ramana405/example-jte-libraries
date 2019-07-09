c192e930311e2ade54e99d879ecd42610ceb3edf
ramana405 <41909526+ramana405@users.noreply.github.com>
2019-07-09 14:50:58 +0530

    Update build.groovy

--- build/build.groovy
+++ build/build.groovy
@@ -4,7 +4,9 @@ stage('build') {
        node {
               echo "Code build is in progress" 
               input id: 'Abc', message: 'Enter the solution file name?', ok: 'Continue', parameters: [string(defaultValue: 'DancingGoat.sln', description: '', name: 'Solution', trim: false)]
+              parameters: [[$class: 'StringParameterValue', name: 'target', value: target], [$class: 'ListSubversionTagsParameterValue', name: 'release', tag: release], [$class: 'BooleanParameterValue', name: 'update_composer', value: update_composer]]
               echo "${params.Solution}"
+              echo "${params.release}"
               echo "${env.JOB_NAME}"
               //bat '"C:\\ProgramData\\chocolatey\\lib\\NuGet.CommandLine\\tools\\nuget.exe" restore DancingGoat.sln'
               //bat "\"${tool 'MSBuild'}\" DancingGoat.sln /p:Configuration=Release /p:Platform=\"Any CPU\" /P:DeployOnBuild=true"
