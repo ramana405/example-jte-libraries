662a20a1c1f91ca4d6199b8c6d78e94bfb064676
ramana405 <41909526+ramana405@users.noreply.github.com>
2019-07-09 20:04:22 +0530

    Update build.groovy

--- build/build.groovy
+++ build/build.groovy
@@ -1,5 +1,9 @@
+import static Constants.*
 void call() {
 echo "starting msbuild"
+class Constants {
+    static final MYCONSTANT
+}       
 stage('build') {
        node {
               echo "Code build is in progress"
@@ -8,13 +12,14 @@ stage('build') {
   		              string(name: 'Solution_file', defaultValue: 'undefined')
   	              ])
               ])
+              ${MYCONSTANT}=${params.Solution_file}    
               println "####Printing parameter:"
               echo "mybranch = ${params.Solution_file}"
               echo "${env.JOB_URL}"
-              bat "\"C:\\ProgramData\\chocolatey\\lib\\NuGet.CommandLine\\tools\\nuget.exe\" restore ${params.Solution_file}"
-              bat "\"${tool 'MSBuild'}\" ${params.Solution_file} /T:Clean;Build /p:Configuration=Release /p:PrecompileBeforePublish=true /p:PackageAsSingleFile=true"
+              //bat "\"C:\\ProgramData\\chocolatey\\lib\\NuGet.CommandLine\\tools\\nuget.exe\" restore ${params.Solution_file}"
+              //bat "\"${tool 'MSBuild'}\" ${params.Solution_file} /T:Clean;Build /p:Configuration=Release /p:PrecompileBeforePublish=true /p:PackageAsSingleFile=true"
               //bat "\"${tool 'MSBuild'}\" ${params.Solution_file} /T:Clean;Build;package /p:Configuration=Release /p:OutputPath=obj\\Release /p:PrecompileBeforePublish=true /p:PackageAsSingleFile=true"       
        }
-  } 
+  }    
 echo "ending msbuild"
 }
