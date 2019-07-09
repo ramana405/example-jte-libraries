af4e31e725a5af2695d59434eb8c0e7add40ed58
ramana405 <41909526+ramana405@users.noreply.github.com>
2019-07-09 12:49:06 +0530

    Update build.groovy

--- build/build.groovy
+++ build/build.groovy
@@ -5,6 +5,7 @@ stage('build') {
               echo "Code build is in progress" 
               input id: 'Abc', message: 'enter build file name?', parameters: [string(defaultValue: 'DemoApp.sln', description: '', name: 'build_file', trim: false)]
               echo "${Abc.build_file}"
+              echo "${env.JOB_NAME}"
               //bat '"C:\\ProgramData\\chocolatey\\lib\\NuGet.CommandLine\\tools\\nuget.exe" restore DancingGoat.sln'
               //bat "\"${tool 'MSBuild'}\" DancingGoat.sln /p:Configuration=Release /p:Platform=\"Any CPU\" /P:DeployOnBuild=true"
        }
