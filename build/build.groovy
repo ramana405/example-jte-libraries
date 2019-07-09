17a54ba892f27ed7be5a8c98ebb4ae521b29a198
ramana405 <41909526+ramana405@users.noreply.github.com>
2019-07-09 18:13:36 +0530

    Update build.groovy

--- build/build.groovy
+++ build/build.groovy
@@ -12,7 +12,8 @@ stage('build') {
               echo "mybranch = ${params.Solution_file}"
               echo "${env.JOB_URL}"
               bat "\"C:\\ProgramData\\chocolatey\\lib\\NuGet.CommandLine\\tools\\nuget.exe\" restore ${params.Solution_file}"
-              bat "\"${tool 'MSBuild'}\" ${params.Solution_file} /T:Clean;Build;Package /p:Configuration=Release /p:OutputPath=obj\\Release /p:PrecompileBeforePublish=true /p:PackageAsSingleFile=true"
+              bat "\"${tool 'MSBuild'}\" ${params.Solution_file} /T:Clean;Build; /p:Configuration=Release /p:PrecompileBeforePublish=true /p:PackageAsSingleFile=true"
+              //bat "\"${tool 'MSBuild'}\" ${params.Solution_file} /T:Clean;Build; /p:Configuration=Release /p:OutputPath=obj\\Release /p:PrecompileBeforePublish=true /p:PackageAsSingleFile=true"       
        }
   } 
 echo "ending msbuild"
