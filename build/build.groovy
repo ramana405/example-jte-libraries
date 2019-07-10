f972448ccc4c44fa3445a7c770db1523d63c06d7
ramana405 <41909526+ramana405@users.noreply.github.com>
2019-07-10 12:21:48 +0530

    Update build.groovy

--- build/build.groovy
+++ build/build.groovy
@@ -16,8 +16,8 @@ stage('build') {
               println "####Printing parameter:"
               echo "mybranch = ${params.Solution_file}"
               echo "${env.JOB_URL}"
-              //bat "\"C:\\ProgramData\\chocolatey\\lib\\NuGet.CommandLine\\tools\\nuget.exe\" restore ${params.Solution_file}"
-              //bat "\"${tool 'MSBuild'}\" ${params.Solution_file} /T:Clean;Build /p:Configuration=Release /p:PrecompileBeforePublish=true /p:PackageAsSingleFile=true"
+              bat "\"C:\\ProgramData\\chocolatey\\lib\\NuGet.CommandLine\\tools\\nuget.exe\" restore ${params.Solution_file}"
+              bat "\"${tool 'MSBuild'}\" ${params.Solution_file} /T:Clean;Build /p:Configuration=Release /p:PrecompileBeforePublish=true /p:PackageAsSingleFile=true"
               //bat "\"${tool 'MSBuild'}\" ${params.Solution_file} /T:Clean;Build;package /p:Configuration=Release /p:OutputPath=obj\\Release /p:PrecompileBeforePublish=true /p:PackageAsSingleFile=true"       
        }
   }    
