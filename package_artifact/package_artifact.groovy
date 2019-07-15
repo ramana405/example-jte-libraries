7593931aa99d28cffe7baf15199d1fdf70f00829
ramana405 <41909526+ramana405@users.noreply.github.com>
2019-07-15 19:03:12 +0530

    Update package_artifact.groovy

--- package_artifact/package_artifact.groovy
+++ package_artifact/package_artifact.groovy
@@ -12,8 +12,8 @@ stage("Package"){
                             string(name: 'Project_Name', defaultValue: 'undefined')
   	              ])
               ])
-              bat "\"${tool 'MSBuild'}\" ${params.Solution_file} /T:Clean;Build;package /p:Configuration=Release /p:OutputPath=obj\\Release /p:PrecompileBeforePublish=true /p:PackageAsSingleFile=true"
-              bat "rename C:\\Users\\venkataramana.t\\.jenkins\\workspace\\${env.JOB_NAME}\\${params.Project_Name}\\obj\\Release\\Package\\${params.Project_Name}.zip ${params.Project_Name}-${env.BUILD_NUMBER}.zip"
+              //bat "\"${tool 'MSBuild'}\" ${params.Solution_file} /T:Clean;Build;package /p:Configuration=Release /p:OutputPath=obj\\Release /p:PrecompileBeforePublish=true /p:PackageAsSingleFile=true"
+              //bat "rename C:\\Users\\venkataramana.t\\.jenkins\\workspace\\${env.JOB_NAME}\\${params.Project_Name}\\obj\\Release\\Package\\${params.Project_Name}.zip ${params.Project_Name}-${env.BUILD_NUMBER}.zip"
        }
 }
 echo "Ending Package Stage"      
