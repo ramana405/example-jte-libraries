5ab3371927aa2d282b8358d87ee4517f35d6a51c
ramana405 <41909526+ramana405@users.noreply.github.com>
2019-07-22 18:57:01 +0530

    Update deploy.groovy

--- deploy/deploy.groovy
+++ deploy/deploy.groovy
@@ -4,7 +4,7 @@ echo "Starting Deployment"
 stage("Deploy"){
     node{
         sleep(5)
-	//"c:\Program Files (x86)\IIS\Microsoft Web Deploy V3\msdeploy.exe" -verb:sync -source:package=dancing.zip -dest:auto,computerName=localhost    
+	//"c:\Program Files (x86)\IIS\Microsoft Web Deploy V3\msdeploy.exe" -verb:sync -source:package=dancing.zip -dest:auto,computerName=https://192.168.205.10    
     }
 }
 echo "Auto deployment done"	
