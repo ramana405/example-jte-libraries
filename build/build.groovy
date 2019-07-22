99ee1f3e90ace9d4a2d3846eaa66f14f5347dfa8
ramana405 <41909526+ramana405@users.noreply.github.com>
2019-07-22 18:53:54 +0530

    Update build.groovy

--- build/build.groovy
+++ build/build.groovy
@@ -12,16 +12,8 @@ stage('build') {
                             string(name: 'Project_Name1', defaultValue: 'undefined')
   	              ])
               ])
-              //MYCONSTANT = "${params.Solution_file}"    
-              println "####Printing parameter:"
-              echo "mybranch = ${params.Solution_file}"
-              echo "${env.BUILD_URL}"
-              echo "${env.JOB_NAME}////name of job"
-              echo "${env.JOB_BASE_NAME}////base name of job"
-              echo "${env.BRANCH_NAME}////env Branch name"
-             // bat "\"C:\\ProgramData\\chocolatey\\lib\\NuGet.CommandLine\\tools\\nuget.exe\" restore ${params.Solution_file}"
-              //bat "\"${tool 'MSBuild'}\" ${params.Solution_file} /T:Clean;Build /p:Configuration=Release /p:PrecompileBeforePublish=true /p:PackageAsSingleFile=true"
-              //bat "\"${tool 'MSBuild'}\" ${params.Solution_file} /T:Clean;Build;package /p:Configuration=Release /p:OutputPath=obj\\Release /p:PrecompileBeforePublish=true /p:PackageAsSingleFile=true"       
+              bat "\"C:\\ProgramData\\chocolatey\\lib\\NuGet.CommandLine\\tools\\nuget.exe\" restore ${params.Solution_file}"
+              bat "\"${tool 'MSBuild'}\" ${params.Solution_file} /T:Clean;Build /p:Configuration=Release /p:PrecompileBeforePublish=true /p:PackageAsSingleFile=true"           
        }
   }    
 echo "ending msbuild"
