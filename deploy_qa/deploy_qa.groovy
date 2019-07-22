7d3224323325dc00bacc3d4fad02990aaadff974
ramana405 <41909526+ramana405@users.noreply.github.com>
2019-07-22 18:58:28 +0530

    Update deploy_qa.groovy

--- deploy_qa/deploy_qa.groovy
+++ deploy_qa/deploy_qa.groovy
@@ -5,11 +5,10 @@ stage("Deploy_QA"){
 	node{
 	     echo "Deploying to QA"
 	     sleep(5)	
-              /*mail to: 'venkataramana.thalari@cummins.com',
+              mail to: 'venkataramana.thalari@cummins.com',
               subject: "Job '${env.JOB_NAME}' (${env.BUILD_NUMBER}) is waiting for input",
               body: "Please go to ${env.BUILD_URL}console and verify the build"
-              //input "Deploy to Production?"
-	      input id: 'Abc', message: 'Deploy to QA?'	*/
+              input "Deploy to Production?"
 	      //Deployment Script
     }
 }
