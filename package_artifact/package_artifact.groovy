d6815a7ffdf0eb0ba20d17325b6c4e3e697c072f
ramana405 <41909526+ramana405@users.noreply.github.com>
2019-07-22 18:56:01 +0530

    Update package_artifact.groovy

--- package_artifact/package_artifact.groovy
+++ package_artifact/package_artifact.groovy
@@ -3,17 +3,14 @@ echo "Starting Package Stage"
 stage("Package"){
        node{
               echo "packaging the code is in progress.."
-              echo "${env.JOB_BASE_NAME}"
-              echo "${env.JOB_NAME}"
-              echo "${env.PROJECT_NAME}"
               properties([
   	              parameters([
                             string(name: 'Solution_file', defaultValue: 'undefined'),
                             string(name: 'Project_Name1', defaultValue: 'undefined')
   	              ])
               ])
-              //bat "\"${tool 'MSBuild'}\" ${params.Solution_file} /T:Clean;Build;package /p:Configuration=Release /p:OutputPath=obj\\Release /p:PrecompileBeforePublish=true /p:PackageAsSingleFile=true"
-              //bat "rename C:\\Users\\venkataramana.t\\.jenkins\\workspace\\${env.JOB_NAME}\\${params.Project_Name}\\obj\\Release\\Package\\${params.Project_Name}.zip ${params.Project_Name}-${env.BUILD_NUMBER}.zip"
+              bat "\"${tool 'MSBuild'}\" ${params.Solution_file} /T:Clean;Build;package /p:Configuration=Release /p:OutputPath=obj\\Release /p:PrecompileBeforePublish=true /p:PackageAsSingleFile=true"
+              bat "rename C:\\Users\\venkataramana.t\\.jenkins\\workspace\\${env.JOB_NAME}\\${params.Project_Name}\\obj\\Release\\Package\\${params.Project_Name}.zip ${params.Project_Name}-${env.BUILD_NUMBER}.zip"
        }
 }
 echo "Ending Package Stage"      
